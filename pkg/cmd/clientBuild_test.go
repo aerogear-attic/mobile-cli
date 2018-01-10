7008cf861a3aae40601747aea70c40c69112b347
Vitalii Chepeliuk <vchepeli@redhat.com>
2018-01-10 15:05:43 +0100

    Update unit test for clientBuild.go

--- pkg/cmd/clientBuild_test.go
+++ pkg/cmd/clientBuild_test.go
@@ -2,10 +2,29 @@ package cmd
 
 import (
 	"reflect"
+	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/spf13/cobra"
 )
+
+func funcName(v reflect.Value, stripFm bool) (pkg, name string) {
+	var pkgRes, nameRes string
+	if v.Kind() == reflect.Func { // check if function is passed
+		name = runtime.FuncForPC(v.Pointer()).Name()
+		if i := strings.LastIndex(name, "."); i != -1 {
+			pkgRes, nameRes = name[:i], name[i+1:]
+		}
+	}
+
+	if stripFm { // remove -fm suffix if true
+		fmPos := strings.LastIndex(nameRes, "-fm")
+		nameRes = nameRes[:fmPos]
+	}
+
+	return pkgRes, nameRes
+}
 
 func TestNewClientBuildsCmd(t *testing.T) {
 	tests := []struct {
@@ -28,13 +47,15 @@ func TestNewClientBuildsCmd(t *testing.T) {
 
 func TestClientBuildsCmd_GetClientBuildsCmd(t *testing.T) {
 	tests := []struct {
-		name string
-		cbc  *ClientBuildsCmd
-		want *cobra.Command
+		name           string
+		cbcClosureFunc func(cbc *ClientBuildsCmd) func() *cobra.Command
+		want           *cobra.Command
 	}{
 		{
 			name: "test get client builds command",
-			cbc:  &ClientBuildsCmd{},
+			cbcClosureFunc: func(cbc *ClientBuildsCmd) func() *cobra.Command {
+				return cbc.GetClientBuildsCmd
+			},
 			want: &cobra.Command{
 				Use:   "clientbuilds",
 				Short: "get clientbuilds for a mobile client",
@@ -43,35 +64,11 @@ func TestClientBuildsCmd_GetClientBuildsCmd(t *testing.T) {
 				},
 			},
 		},
-	}
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			got := tc.cbc.GetClientBuildsCmd()
-			if use := got.Use; !reflect.DeepEqual(use, tc.want.Use) {
-				t.Errorf("ClientBuildsCmd.GetClientBuildsCmd().Use = %v, want %v", use, tc.want.Use)
-			}
-			if short := got.Short; !reflect.DeepEqual(short, tc.want.Short) {
-				t.Errorf("ClientBuildsCmd.GetClientBuildsCmd().Short = %v, want %v", short, tc.want.Short)
-			}
-			if runE := got.RunE; !reflect.DeepEqual(reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE)) {
-				t.Errorf("ClientBuildsCmd.GetClientBuildsCmd().RunE = %v, want %v", reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE))
-			}
-			if runE := got.RunE; !reflect.DeepEqual(runE(nil, nil), tc.want.RunE(nil, nil)) {
-				t.Errorf("ClientBuildsCmd.GetClientBuildsCmd().RunE = %v, want %v", runE(nil, nil), tc.want.RunE(nil, nil))
-			}
-		})
-	}
-}
-
-func TestClientBuildsCmd_ListClientBuildsCmd(t *testing.T) {
-	tests := []struct {
-		name string
-		cbc  *ClientBuildsCmd
-		want *cobra.Command
-	}{
 		{
 			name: "test list client builds command",
-			cbc:  &ClientBuildsCmd{},
+			cbcClosureFunc: func(cbc *ClientBuildsCmd) func() *cobra.Command {
+				return cbc.ListClientBuildsCmd
+			},
 			want: &cobra.Command{
 				Use:   "clientbuild",
 				Short: "get a specific clientbuild for a mobile client",
@@ -80,35 +77,11 @@ func TestClientBuildsCmd_ListClientBuildsCmd(t *testing.T) {
 				},
 			},
 		},
-	}
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			got := tc.cbc.ListClientBuildsCmd()
-			if use := got.Use; !reflect.DeepEqual(use, tc.want.Use) {
-				t.Errorf("ClientBuildsCmd.ListClientBuildsCmd().Use = %v, want %v", use, tc.want.Use)
-			}
-			if short := got.Short; !reflect.DeepEqual(short, tc.want.Short) {
-				t.Errorf("ClientBuildsCmd.ListClientBuildsCmd().Short = %v, want %v", short, tc.want.Short)
-			}
-			if runE := got.RunE; !reflect.DeepEqual(reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE)) {
-				t.Errorf("ClientBuildsCmd.ListClientBuildsCmd().RunE = %v, want %v", reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE))
-			}
-			if runE := got.RunE; !reflect.DeepEqual(runE(nil, nil), tc.want.RunE(nil, nil)) {
-				t.Errorf("ClientBuildsCmd.ListClientBuildsCmd().RunE = %v, want %v", runE(nil, nil), tc.want.RunE(nil, nil))
-			}
-		})
-	}
-}
-
-func TestClientBuildsCmd_CreateClientBuildsCmd(t *testing.T) {
-	tests := []struct {
-		name string
-		cbc  *ClientBuildsCmd
-		want *cobra.Command
-	}{
 		{
 			name: "test create client builds command",
-			cbc:  &ClientBuildsCmd{},
+			cbcClosureFunc: func(cbc *ClientBuildsCmd) func() *cobra.Command {
+				return cbc.CreateClientBuildsCmd
+			},
 			want: &cobra.Command{
 				Use:   "clientbuild",
 				Short: "create a build for a mobile client",
@@ -117,35 +90,11 @@ func TestClientBuildsCmd_CreateClientBuildsCmd(t *testing.T) {
 				},
 			},
 		},
-	}
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			got := tc.cbc.CreateClientBuildsCmd()
-			if use := got.Use; !reflect.DeepEqual(use, tc.want.Use) {
-				t.Errorf("ClientBuildsCmd.CreateClientBuildsCmd().Use = %v, want %v", use, tc.want.Use)
-			}
-			if short := got.Short; !reflect.DeepEqual(short, tc.want.Short) {
-				t.Errorf("ClientBuildsCmd.CreateClientBuildsCmd().Short = %v, want %v", short, tc.want.Short)
-			}
-			if runE := got.RunE; !reflect.DeepEqual(reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE)) {
-				t.Errorf("ClientBuildsCmd.CreateClientBuildsCmd().RunE = %v, want %v", reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE))
-			}
-			if runE := got.RunE; !reflect.DeepEqual(runE(nil, nil), tc.want.RunE(nil, nil)) {
-				t.Errorf("ClientBuildsCmd.CreateClientBuildsCmd().RunE = %v, want %v", runE(nil, nil), tc.want.RunE(nil, nil))
-			}
-		})
-	}
-}
-
-func TestClientBuildsCmd_DeleteClientBuildsCmd(t *testing.T) {
-	tests := []struct {
-		name string
-		cbc  *ClientBuildsCmd
-		want *cobra.Command
-	}{
 		{
-			name: "test create client builds command",
-			cbc:  &ClientBuildsCmd{},
+			name: "test delete client builds command",
+			cbcClosureFunc: func(cbc *ClientBuildsCmd) func() *cobra.Command {
+				return cbc.DeleteClientBuildsCmd
+			},
 			want: &cobra.Command{
 				Use:   "clientbuild",
 				Short: "delete a build for a mobile client",
@@ -154,36 +103,11 @@ func TestClientBuildsCmd_DeleteClientBuildsCmd(t *testing.T) {
 				},
 			},
 		},
-	}
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			//cbc := &ClientBuildsCmd{}
-			got := tc.cbc.DeleteClientBuildsCmd()
-			if use := got.Use; !reflect.DeepEqual(use, tc.want.Use) {
-				t.Errorf("ClientBuildsCmd.DeleteClientBuildsCmd().Use = %v, want %v", use, tc.want.Use)
-			}
-			if short := got.Short; !reflect.DeepEqual(short, tc.want.Short) {
-				t.Errorf("ClientBuildsCmd.DeleteClientBuildsCmd().Short = %v, want %v", short, tc.want.Short)
-			}
-			if runE := got.RunE; !reflect.DeepEqual(reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE)) {
-				t.Errorf("ClientBuildsCmd.DeleteClientBuildsCmd().RunE = %v, want %v", reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE))
-			}
-			if runE := got.RunE; !reflect.DeepEqual(runE(nil, nil), tc.want.RunE(nil, nil)) {
-				t.Errorf("ClientBuildsCmd.DeleteClientBuildsCmd().RunE = %v, want %v", runE(nil, nil), tc.want.RunE(nil, nil))
-			}
-		})
-	}
-}
-
-func TestClientBuildsCmd_StopClientBuildsCmd(t *testing.T) {
-	tests := []struct {
-		name string
-		cbc  *ClientBuildsCmd
-		want *cobra.Command
-	}{
 		{
-			name: "test create client builds command",
-			cbc:  &ClientBuildsCmd{},
+			name: "test stop client builds command",
+			cbcClosureFunc: func(cbc *ClientBuildsCmd) func() *cobra.Command {
+				return cbc.StopClientBuildsCmd
+			},
 			want: &cobra.Command{
 				Use:   "clientbuild",
 				Short: "stop a build for a mobile client",
@@ -192,35 +116,11 @@ func TestClientBuildsCmd_StopClientBuildsCmd(t *testing.T) {
 				},
 			},
 		},
-	}
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			got := tc.cbc.StopClientBuildsCmd()
-			if use := got.Use; !reflect.DeepEqual(use, tc.want.Use) {
-				t.Errorf("ClientBuildsCmd.StopClientBuildsCmd().Use = %v, want %v", use, tc.want.Use)
-			}
-			if short := got.Short; !reflect.DeepEqual(short, tc.want.Short) {
-				t.Errorf("ClientBuildsCmd.StopClientBuildsCmd().Short = %v, want %v", short, tc.want.Short)
-			}
-			if runE := got.RunE; !reflect.DeepEqual(reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE)) {
-				t.Errorf("ClientBuildsCmd.StopClientBuildsCmd().RunE = %v, want %v", reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE))
-			}
-			if runE := got.RunE; !reflect.DeepEqual(runE(nil, nil), tc.want.RunE(nil, nil)) {
-				t.Errorf("ClientBuildsCmd.StopClientBuildsCmd().RunE = %v, want %v", runE(nil, nil), tc.want.RunE(nil, nil))
-			}
-		})
-	}
-}
-
-func TestClientBuildsCmd_StartClientBuildsCmd(t *testing.T) {
-	tests := []struct {
-		name string
-		cbc  *ClientBuildsCmd
-		want *cobra.Command
-	}{
 		{
-			name: "test create client builds command",
-			cbc:  &ClientBuildsCmd{},
+			name: "test start client builds command",
+			cbcClosureFunc: func(cbc *ClientBuildsCmd) func() *cobra.Command {
+				return cbc.StartClientBuildsCmd
+			},
 			want: &cobra.Command{
 				Use:   "clientbuild",
 				Short: "start a build for a mobile client",
@@ -232,19 +132,22 @@ func TestClientBuildsCmd_StartClientBuildsCmd(t *testing.T) {
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			//cbc := &ClientBuildsCmd{}
-			got := tc.cbc.StartClientBuildsCmd()
+			cbc := &ClientBuildsCmd{}
+			gotFunc := tc.cbcClosureFunc(cbc)
+			_, gotFuncName := funcName(reflect.ValueOf(gotFunc), true)
+
+			got := gotFunc()
 			if use := got.Use; !reflect.DeepEqual(use, tc.want.Use) {
-				t.Errorf("ClientBuildsCmd.StartClientBuildsCmd().Use = %v, want %v", use, tc.want.Use)
+				t.Errorf("ClientBuildsCmd.%v().Use = %v, want %v", gotFuncName, use, tc.want.Use)
 			}
 			if short := got.Short; !reflect.DeepEqual(short, tc.want.Short) {
-				t.Errorf("ClientBuildsCmd.StartClientBuildsCmd().Short = %v, want %v", short, tc.want.Short)
+				t.Errorf("ClientBuildsCmd.%v().Short = %v, want %v", gotFuncName, short, tc.want.Short)
 			}
 			if runE := got.RunE; !reflect.DeepEqual(reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE)) {
-				t.Errorf("ClientBuildsCmd.StartClientBuildsCmd().RunE = %v, want %v", reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE))
+				t.Errorf("ClientBuildsCmd.%v().RunE = %v, want %v", gotFuncName, reflect.TypeOf(runE), reflect.TypeOf(tc.want.RunE))
 			}
 			if runE := got.RunE; !reflect.DeepEqual(runE(nil, nil), tc.want.RunE(nil, nil)) {
-				t.Errorf("ClientBuildsCmd.StartClientBuildsCmd().RunE = %v, want %v", runE(nil, nil), tc.want.RunE(nil, nil))
+				t.Errorf("ClientBuildsCmd.%v().RunE = %v, want %v", gotFuncName, runE(nil, nil), tc.want.RunE(nil, nil))
 			}
 		})
 	}
