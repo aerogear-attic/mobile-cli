4683c0f96bf7b51f07adfebf9c685fc37e7503c6
witmicko <mogrodni@redhat.com>
2018-01-17 11:53:00 +0000

    trim actual and expected string before compare

--- integration/client_get_services_test.go
+++ integration/client_get_services_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"regexp"
 	"testing"
 )
 
@@ -16,6 +17,13 @@ var update = flag.Bool("update", false, "update golden files")
 const testPath = "getServicesTestData/"
 
 func TestGetServices(t *testing.T) {
+	//regexes to match dynamic properties in service json, UID resourceVersion and creationTimestamp
+	regexes := []*regexp.Regexp{
+		regexp.MustCompile("\"uid\".*?,"),
+		regexp.MustCompile("\"resourceVersion\".*?,"),
+		regexp.MustCompile("\"creationTimestamp\".*?,"),
+	}
+
 	tests := []struct {
 		name    string
 		args    []string
@@ -38,6 +46,7 @@ func TestGetServices(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
+
 			if *update {
 				WriteSnapshot(t, testPath+test.fixture, output)
 			}
@@ -46,12 +55,24 @@ func TestGetServices(t *testing.T) {
 
 			expected := LoadSnapshot(t, testPath+test.fixture)
 
+			if test.name == "json output" {
+				actual = cleanStringByRegex(actual, regexes)
+				expected = cleanStringByRegex(expected, regexes)
+			}
+
 			if actual != expected {
 				t.Fatalf("actual = \n%s, expected = \n%s", actual, expected)
 			}
 		})
 	}
 
+}
+
+func cleanStringByRegex(input string, regexes []*regexp.Regexp) string {
+	for _, regex := range regexes {
+		input = regex.ReplaceAllString(input, "")
+	}
+	return input
 }
 
 func TestMain(m *testing.M) {
