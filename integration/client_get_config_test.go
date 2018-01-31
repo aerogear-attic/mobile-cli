085b2f1cdbe822ac561db8af879bdf083040a194
Jameel Briones <jbriones@redhat.com>
2018-01-31 14:25:14 +0000

    create integration tests for get clientconfig command

---
+++ integration/client_get_config_test.go
@@ -0,0 +1,94 @@
+package integration
+
+import (
+	"os/exec"
+	"regexp"
+	"testing"
+)
+
+type Client struct {
+	ID        string
+	Name      string
+	Type      string
+	Namespace string
+	BundleID  string
+}
+
+// Create struct for MobileClientConfig?
+const getClientTestPath = "getClientConfigTestData/"
+
+func TestGetClientConfig(t *testing.T) {
+	//regexes to match dynamic properties in the client config
+	regexes := []*regexp.Regexp{
+		regexp.MustCompile("\"cluster_name\".*?,"),
+	}
+
+	client := &Client{
+		ID:        "test-client-cordova",
+		Name:      "test-client",
+		Type:      "cordova",
+		Namespace: "--namespace=myproject",
+		BundleID:  "test-bundle-id",
+	}
+
+	createTestClient(t, client)
+
+	tests := []struct {
+		name    string
+		args    []string
+		fixture string
+	}{
+		{"json output", []string{"get", "clientconfig", client.ID, client.Namespace, "-o=json"}, "json-output.golden"},
+		{"table output", []string{"get", "clientconfig", client.ID, client.Namespace, "-o=table"}, "table-output.golden"},
+		{"no clientID", []string{"get", "clientconfig", client.Namespace}, "no-client-id.golden"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			cmd := exec.Command(*executable, test.args...)
+
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			actual := string(output)
+			expected := LoadSnapshot(t, getClientTestPath+test.fixture)
+
+			if *update {
+				WriteSnapshot(t, getClientTestPath+test.fixture, output)
+			}
+
+			if test.name == "json output" {
+				actual = CleanStringByRegex(actual, regexes)
+				expected = CleanStringByRegex(expected, regexes)
+			}
+
+			if actual != expected {
+				t.Fatalf("actual = \n%s, expected = \n%s", actual, expected)
+			}
+		})
+	}
+
+	deleteTestClient(t, client)
+}
+
+func createTestClient(t *testing.T, client *Client) {
+	createClientCmdArgs := []string{"create", "client", client.Name, client.Type, client.BundleID, client.Namespace}
+	createClientCmd := exec.Command(*executable, createClientCmdArgs...)
+
+	_, err := createClientCmd.CombinedOutput()
+	if err != nil {
+		t.Fatal("Failed to create client: ", err)
+	}
+}
+
+func deleteTestClient(t *testing.T, client *Client) {
+	deleteClientCmdArgs := []string{"delete", "client", client.ID, client.Namespace}
+	deleteClientCmd := exec.Command(*executable, deleteClientCmdArgs...)
+
+	_, err := deleteClientCmd.CombinedOutput()
+	if err != nil {
+		t.Fatal("Failed to delete client: ", err)
+	}
+}
--- integration/client_get_services_test.go
+++ integration/client_get_services_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-const testPath = "getServicesTestData/"
+const getServicesTestPath = "getServicesTestData/"
 
 func TestGetServices(t *testing.T) {
 	//regexes to match dynamic properties in service json, UID resourceVersion and creationTimestamp
@@ -35,16 +35,16 @@ func TestGetServices(t *testing.T) {
 				t.Fatal(err)
 			}
 			if *update {
-				WriteSnapshot(t, testPath+test.fixture, output)
+				WriteSnapshot(t, getServicesTestPath+test.fixture, output)
 			}
 
 			actual := string(output)
 
-			expected := LoadSnapshot(t, testPath+test.fixture)
+			expected := LoadSnapshot(t, getServicesTestPath+test.fixture)
 
 			if test.name == "json output" {
-				actual = cleanStringByRegex(actual, regexes)
-				expected = cleanStringByRegex(expected, regexes)
+				actual = CleanStringByRegex(actual, regexes)
+				expected = CleanStringByRegex(expected, regexes)
 			}
 
 			if actual != expected {
@@ -52,12 +52,4 @@ func TestGetServices(t *testing.T) {
 			}
 		})
 	}
-
-}
-
-func cleanStringByRegex(input string, regexes []*regexp.Regexp) string {
-	for _, regex := range regexes {
-		input = regex.ReplaceAllString(input, "")
-	}
-	return input
 }
